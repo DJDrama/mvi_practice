695621eff36ba9bcdbfab67dc4a485d92eb3a251
DJ <putzzang@gmail.com>
2019-11-26 16:39:40 +0900

    Custom Bottom Nav Controller

--- app/src/main/java/com/codingwithmitch/openapi/ui/main/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/openapi/ui/main/MainActivity.kt
@@ -3,32 +3,65 @@ package com.codingwithmitch.openapi.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
 import com.codingwithmitch.openapi.R
 import com.codingwithmitch.openapi.ui.BaseActivity
 import com.codingwithmitch.openapi.ui.auth.AuthActivity
+import com.codingwithmitch.openapi.ui.main.account.ChangePasswordFragment
+import com.codingwithmitch.openapi.ui.main.account.UpdateAccountFragment
+import com.codingwithmitch.openapi.ui.main.blog.UpdateBlogFragment
+import com.codingwithmitch.openapi.ui.main.blog.ViewBlogFragment
+import com.codingwithmitch.openapi.util.BottomNavController
+import com.codingwithmitch.openapi.util.BottomNavController.*
+import com.codingwithmitch.openapi.util.setUpNavigation
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: BaseActivity() {
+class MainActivity : BaseActivity(),
+    NavGraphProvider,
+    OnNavigationGraphChanged,
+    OnNavigationReselectedListener {
+
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
+        BottomNavController(
+            this,
+            R.id.main_nav_host_fragment,
+            R.id.nav_blog, //start Destination
+            this,
+            this
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupActionBar()
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view)
+        bottomNavigationView.setUpNavigation(bottomNavController, this)
+        if(savedInstanceState == null){
+            bottomNavController.onNavigationItemSelected()
+        }
 
         subscribeObservers()
     }
 
-    fun subscribeObservers(){
-        sessionManager.cachedToken.observe(this, Observer{authToken->
+    fun subscribeObservers() {
+        sessionManager.cachedToken.observe(this, Observer { authToken ->
             Log.d(TAG, "MainActivity: subsribeOBservers: AuthToken : ${authToken}")
-            if(authToken == null || authToken.account_pk == -1 || authToken.token == null){
+            if (authToken == null || authToken.account_pk == -1 || authToken.token == null) {
                 navAuthActivity()
             }
         })
     }
-    private fun navAuthActivity(){
+
+    private fun navAuthActivity() {
         val intent = Intent(this, AuthActivity::class.java)
         startActivity(intent)
         finish()
@@ -42,4 +75,55 @@ class MainActivity: BaseActivity() {
         }
     }
 
+    override fun getNavGraphId(itemId: Int) = when (itemId) {
+        R.id.nav_blog -> {
+            R.navigation.nav_blog
+        }
+        R.id.nav_create_blog -> {
+            R.navigation.nav_create_blog
+        }
+        R.id.nav_account -> {
+            R.navigation.nav_account
+        }
+        else ->{
+            R.navigation.nav_blog
+        }
+    }
+
+    override fun onGraphChange() {
+        //What needs to happen when the graph changes? ex) canceling active jobs
+    }
+
+    override fun onReselectNavItem(navController: NavController, fragment: Fragment) = when(fragment){
+        is ViewBlogFragment ->{
+            navController.navigate(R.id.action_viewBlogFragment_to_blogFragment)
+        }
+        is UpdateBlogFragment ->{
+            navController.navigate(R.id.action_updateBlogFragment_to_blogFragment)
+        }
+        is UpdateAccountFragment ->{
+            navController.navigate(R.id.action_updateAccountFragment_to_accountFragment)
+        }
+        is ChangePasswordFragment ->{
+            navController.navigate(R.id.action_changePasswordFragment_to_accountFragment)
+        }
+        else->{
+            //do nothing
+        }
+    }
+
+    override fun onBackPressed() = bottomNavController.onBackPressed()
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            android.R.id.home->{
+                onBackPressed()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(tool_bar)
+    }
 }
