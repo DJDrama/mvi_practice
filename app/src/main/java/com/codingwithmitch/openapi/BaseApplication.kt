201fb92f14c2eee933ba2582d54e5ea734e88e41
DJ <putzzang@gmail.com>
2019-12-30 17:46:13 +0900

    Dagger Refactor

--- app/src/main/java/com/codingwithmitch/openapi/BaseApplication.kt
+++ app/src/main/java/com/codingwithmitch/openapi/BaseApplication.kt
@@ -1,14 +1,35 @@
 package com.codingwithmitch.openapi
 
+import android.app.Activity
+import android.app.Application
+import com.codingwithmitch.openapi.di.AppInjector
 import com.codingwithmitch.openapi.di.DaggerAppComponent
 import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import dagger.android.support.DaggerApplication
+import javax.inject.Inject
 
-class BaseApplication: DaggerApplication() {
+class BaseApplication: Application(), HasAndroidInjector{
 
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerAppComponent.builder().application(this).build()
+    @Inject
+    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    override fun onCreate() {
+        super.onCreate()
+        AppInjector.init(this)
     }
+
+    override fun androidInjector(): AndroidInjector<Any> {
+        return dispatchingAndroidInjector
+    }
+
+//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+//       // return DaggerAppComponent.builder().application(this).build()
+//
+//    }
+
+
 
 
 }
